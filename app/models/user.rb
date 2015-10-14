515efd6ba1e20ab9bbea085238d257c51afb7d49
davidbebb <davidbebb@gmail.com>
2015-10-14 15:35:21 +0100

    added settings to facebook oauth to get email, sign in now works unless unser has very tight privacy settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,26 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-       p 'hi'
-       p auth
        user.email = auth.info.email
+       p auth.info.name
+       p auth.info.email
        user.password = Devise.friendly_token[0,20]
-      #  user.name = auth.extra.name
+       #user.name = auth.info.name
       #  user.image = auth.info.image
      end
    end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def facebook
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+    end
+  end
 end
