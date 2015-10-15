3c888ff1150b778b3c17ecdbfa6b85724ba28471
Jongmin Kim <kjm141215@gmail.com>
2015-10-15 16:37:44 +0100

    tested the presence of delete review link

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,25 +1,36 @@
 require 'rails_helper'
 
-feature 'reviewing' do
+
+
+feature 'review' do
 
   before(:each) do
-    user = FactoryGirl.create(:user)
+    @user1 = create(:user)
     visit '/users/sign_in'
     fill_in 'Email', with: 'test@email.com'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
-    Restaurant.create name: 'KFC'
+    @user1.restaurants.create name: 'KFC'
   end
 
-  scenario 'allows a user to leave a review using a form ' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in "Thoughts", with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+  context 'creating reviews' do
+    scenario 'allows a user to leave a review using a form ' do
+      visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in "Thoughts", with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
 
-    expect(current_path).to eq('/restaurants')
-    expect(page).to have_content('so so')
+      expect(current_path).to eq('/restaurants')
+      expect(page).to have_content('so so')
+    end
   end
+
+  # context 'deleting reviews' do
+  #   scenario 'user can delete their own reviews' do
+  #     @user1.
+  #
+  #   end
+  # end
 
 end
