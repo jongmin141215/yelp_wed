a9bc5ee4d1ba765d1e127ededfafa90ff4458283
Jongmin Kim <kjm141215@gmail.com>
2015-10-16 11:39:51 +0100

    added star_rating helper method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,7 @@
 module ReviewsHelper
+  def star_rating(rating)
+    return rating unless rating.respond_to?(:round)
+    remainder = 5 - rating
+    r = ('★' * rating.round) + ('☆' * remainder)
+  end
 end
