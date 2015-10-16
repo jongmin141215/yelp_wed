83c8f779b5a04b47805d69d3640dfb801a3c53db
Jongmin Kim <kjm141215@gmail.com>
2015-10-16 16:39:31 +0100

    User can only endorse a review once

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,11 +1,16 @@
 class EndorsementsController < ApplicationController
 
+  def index
+    redirect_to restaurants_path
+  end
+
   def create
     @review = Review.find(params[:review_id])
     @endorsement = @review.build_with_user(current_user)
 
     if @endorsement.save
-      redirect_to restaurants_path
+      render json: { new_endorsement_count: @review.endorsements.length }
+
     else
       redirect_to restaurants_path
     end
--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -7,6 +7,7 @@ feature 'endorsing reviews' do
     @user2 = create(:user2)
     kfc = Restaurant.create(name: 'KFC')
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
+    # @review = @user.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
   scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
@@ -15,20 +16,20 @@ feature 'endorsing reviews' do
     expect(page).to have_content '1 endorsement'
   end
 
-  # scenario 'a user can only endorse a review once' do
-  #   visit '/restaurants'
-  #   click_link 'Endorse Review'
-  #   click_link 'Endorse Review'
-  #   expect(page).to have_content '1 endorsement'
-  # end
+  scenario 'a user can only endorse a review once', js: true do
+    visit '/restaurants'
+    click_link 'Endorse Review'
+    click_link 'Endorse Review'
+    expect(page).to have_content '1 endorsement'
+  end
 
-  scenario 'other users can endorse a review that they have not endorsed' do
-    visit 'restaurants'
+  scenario 'other users can endorse a review that they have not endorsed', js: true do
+    visit '/restaurants'
     click_link 'Endorse Review'
     click_link 'Sign out'
     sign_in(@user2)
     click_link 'Endorse Review'
-    expect(page).to have_content '2 endorsements'
+    expect(page).to have_content '2 endorsement'
   end
 
   # scenario 'when a user endorses a review, "Endorse Review" link disappers' do
